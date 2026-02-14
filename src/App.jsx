1efbcd1e767675fe2f702313b0e13c0349ec62a6
manish <ranwamanish285@gmail.com>
2026-02-14 12:49:43 +0530

    header compantes add

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css'
+import Header from './compantes/header/Header'
 
 function App() {
 
   return (
     <>
       <h1>App Component</h1>
+      <Header/>
     </>
   )
 }
---
+++ src/compantes/header/Header.jsx
@@ -0,0 +1,11 @@
+
+
+function Header(){
+    return (
+        <>
+        <h1>header compontes</h1>
+        </>
+    )
+
+}
+export default Header;
